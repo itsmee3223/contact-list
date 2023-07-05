e9495b813fd2da5d45f7926af3d3601309470fe3
itsmee3223 <tamvam.pemberani@gmail.com>
2023-07-05 09:08:22 +0700

    update prisma config

---
+++ contact-list/prisma/migrations/20230705020636_initial/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `Contact` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `firstName` VARCHAR(191) NOT NULL,
+    `lastName` VARCHAR(191) NOT NULL,
+    `phoneNumber` VARCHAR(191) NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
